abf552571f026da879758baef64e4bd1b7e559f4
Decoldest <127795154+Decoldest@users.noreply.github.com>
2025-01-15 20:54:59 -0500

    create react app with login basic page

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import { Container, Paper } from "@mui/material";
+
+export default function Login() {
+  return (
+    <Container maxWidth="sm">
+      <Paper elevation={2}>Login</Paper>
+    </Container>
+  );
+}
