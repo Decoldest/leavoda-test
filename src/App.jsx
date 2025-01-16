870c54de21080475ceeeb1587a6722899407a86f
Decoldest <127795154+Decoldest@users.noreply.github.com>
2025-01-15 21:51:04 -0500

    fetch data from dummy API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 
 function App() {
+  const user = useSelector(selectUser);
+  
   return (
     <>
       <Router>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,43 @@
-import { Container, Paper } from "@mui/material";
+import { Container, Paper, TextField, Button, Box } from "@mui/material";
+import { useDispatch } from "react-redux";
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const url = `https://dummyjson.com/users/filter?key=username&value=${username}`;
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const userData = await response.json();
+      console.log(userData);
+      dispatch(userData?.users[0]);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   return (
     <Container maxWidth="sm">
-      <Paper elevation={2}>Login</Paper>
+      <Paper elevation={2} sx={{ padding: 4 }}>
+        <Box component="form" onSubmit={handleLogin}>
+          <TextField
+            name="username"
+            placeholder="Username"
+            required
+            autoFocus
+            fullWidth
+          />
+          <Button type="submit" fullWidth>
+            Sign In
+          </Button>
+        </Box>
+      </Paper>
     </Container>
   );
 }
